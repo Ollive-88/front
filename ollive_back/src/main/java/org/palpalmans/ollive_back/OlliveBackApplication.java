01fb271044bbc677a8febe3548548a8fb85f83fc
Junhyun An <alloy0301@gmail.com>
2024-03-12 14:12:59 +0900

    Add : new project

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/OlliveBackApplication.java
@@ -0,0 +1,13 @@
+package org.palpalmans.ollive_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlliveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlliveBackApplication.class, args);
+	}
+
+}
