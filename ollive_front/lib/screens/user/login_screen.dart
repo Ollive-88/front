1b48c0962aabb7a7f81e3a442ed0e4d5a4365e39
황준식 <wnstlr0908@naver.com>
2024-03-14 00:09:37 +0900

    Feat: 사용자 입력 폼 생성

--- ollive_front/lib/screens/user/login_screen.dart
+++ ollive_front/lib/screens/user/login_screen.dart
@@ -8,18 +8,68 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  Map<String, dynamic> info = {'id': 'hi', 'password': 'hi'};
+
+  final bool _fromChanged = false; // 폼의 UI 상태 관리
+  // 폼의 현재 상태 관리
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Center(
         child: Padding(
-          padding: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Form(child:),
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                            autofocus: true, // 이 페이지로 이동했을 때, 자동 포커스
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            helperText: 'Required',
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              style: const ButtonStyle(
+                                backgroundColor: MaterialStatePropertyAll(
+                                  Color(0xFF30AF98),
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                '로그인',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               )
             ],
           ),
