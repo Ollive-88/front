a1fcd0e4f8e95d9469b1db38a083f77fbda83411
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-03-13 14:37:59 +0900

    Feat : 임시 코드 삭제, 폰트 테마 적용, 상단 앱바 제거

--- ollive_front/lib/main.dart
+++ ollive_front/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ollive_front/widgets/global/appbar_widget.dart';
 import 'package:ollive_front/widgets/global/navigationbar_widget.dart';
 
 void main() {
@@ -16,19 +15,7 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   int _selectedIndex = 0;
 
-  final List<Widget> _page = [
-    const BoardScreen(),
-    const RecipeScreen(),
-    const ClothseScreen(),
-    const MypageScreen(),
-  ];
-
-  final List<String> _title = [
-    '사람사는 얘기',
-    '레시피 추천',
-    '오늘의 패션',
-    '마이페이지',
-  ];
+  final List<Widget> _page = [];
 
   void _onTapNavBar(int index) {
     setState(() {
@@ -39,13 +26,9 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // theme: ThemeData(fontFamily: 'NanumSquare'),
+      theme: ThemeData(fontFamily: 'NanumSquare'),
       home: Scaffold(
         backgroundColor: const Color(0xFFFFFFFC),
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(50),
-          child: CustomAppbar(title: _title[_selectedIndex]),
-        ),
         bottomNavigationBar: CustomNavigationbar(
           selectedIndex: _selectedIndex,
           onItemTapped: _onTapNavBar,
@@ -53,50 +36,5 @@ class _AppState extends State<App> {
         body: _page[_selectedIndex],
       ),
     );
-  }
-}
-
-class ClothseScreen extends StatelessWidget {
-  const ClothseScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-      '옷',
-      style: TextStyle(fontFamily: 'NanumSquare', fontSize: 50),
-    );
-  }
-}
-
-class RecipeScreen extends StatelessWidget {
-  const RecipeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-      '옷',
-      style: TextStyle(
-          fontFamily: 'NanumSquare',
-          fontSize: 50,
-          fontWeight: FontWeight.),
-    );
-  }
-}
-
-class BoardScreen extends StatelessWidget {
-  const BoardScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text('게시판');
-  }
-}
-
-class MypageScreen extends StatelessWidget {
-  const MypageScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text('마이페이지');
   }
 }
