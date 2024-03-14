76005cf85822750aad85718da6ecbc5c71f1965e
황준식 <wnstlr0908@naver.com>
2024-03-14 17:56:27 +0900

    Mod: 로그인 모델 수정

--- ollive_front/lib/models/user/user_model.dart
+++ ollive_front/lib/models/user/user_model.dart
@@ -1,3 +1,18 @@
 class Login {
-  String id, passWord;
+  late String accountName;
+  late String password;
+  late String userId;
+
+  Login.fromJson(Map<String?, dynamic> json)
+      : accountName = json['accountName'],
+        password = json['password'],
+        userId = json['userId'];
+
+  Login.fromUserInput();
+
+  Map<String, dynamic> toJson() => {
+        'accountName': accountName,
+        'password': password,
+        'userId': userId,
+      };
 }
