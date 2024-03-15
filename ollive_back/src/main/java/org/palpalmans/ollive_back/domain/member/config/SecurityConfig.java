5adb1194c76f324f8450f558e7d3803de78a7632
Junhyun An <alloy0301@gmail.com>
2024-03-15 09:28:27 +0900

    Add : BCryptPasswordEncoder, JwtService

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/config/SecurityConfig.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
@@ -18,6 +19,11 @@ import java.util.List;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/service/JwtService.java
@@ -0,0 +1,10 @@
+package org.palpalmans.ollive_back.domain.member.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JwtService {
+
+}
