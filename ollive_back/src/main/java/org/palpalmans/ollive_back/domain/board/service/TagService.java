8f765addb06b8629ae7008c6c693ce0a5e3c1479
bufflect <bndgc@naver.com>
2024-03-20 23:06:53 +0900

    Feat: TagService-saveTags

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/TagService.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/TagService.java
@@ -1,7 +1,11 @@
 package org.palpalmans.ollive_back.domain.board.service;
 
+import java.util.List;
+
+import org.palpalmans.ollive_back.domain.board.model.entity.Tag;
 import org.palpalmans.ollive_back.domain.board.repository.TagRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +13,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TagService {
 	private final TagRepository tagRepository;
+
+	@Transactional
+	public void saveTags(List<String> tagNames) {
+		tagRepository.saveAll(
+			tagNames.stream()
+				.map(Tag::new)
+				.toList()
+		);
+	}
 }
