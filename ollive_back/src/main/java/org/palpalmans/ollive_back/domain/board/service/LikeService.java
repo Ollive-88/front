7f96fcb6e906ea5538ce1e81cfc4f2ef2b3e5dae
bufflect <bndgc@naver.com>
2024-03-20 23:06:47 +0900

    Feat: LikeService-saveLike

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/LikeService.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/LikeService.java
@@ -1,10 +1,21 @@
 package org.palpalmans.ollive_back.domain.board.service;
 
+import org.palpalmans.ollive_back.domain.board.model.entity.Board;
+import org.palpalmans.ollive_back.domain.board.model.entity.Like;
+import org.palpalmans.ollive_back.domain.board.repository.LikeRepository;
+import org.palpalmans.ollive_back.domain.member.model.entity.Member;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
+	private final LikeRepository likeRepository;
+
+	@Transactional
+	public void saveLike(Board board, Member memeber) {
+		likeRepository.save(new Like(board, memeber));
+	}
 }
