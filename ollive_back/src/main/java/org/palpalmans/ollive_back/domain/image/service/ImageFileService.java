0938ddc660b888fc521473b6233f7661649a9c6f
JuHyeong <bndgc@naver.com>
2024-03-20 13:30:34 +0900

    Add: ImageFileService

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/image/service/ImageFileService.java
@@ -0,0 +1,9 @@
+package org.palpalmans.ollive_back.domain.image.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageFileService {
+}
