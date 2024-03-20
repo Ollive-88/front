dc6267733d3e3b27f92ebf7e05709f4411fd4db7
bufflect <bndgc@naver.com>
2024-03-20 21:59:47 +0900

    Add: BoardMapper

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/model/BoardMapper.java
@@ -0,0 +1,12 @@
+package org.palpalmans.ollive_back.domain.board.model;
+
+import org.palpalmans.ollive_back.domain.board.model.dto.WriteBoardRequest;
+import org.palpalmans.ollive_back.domain.board.model.entity.Board;
+
+public class BoardMapper {
+	public static Board toBoard(WriteBoardRequest writeBoardRequest, Long memberId) {
+		return new Board(
+			writeBoardRequest.title(), writeBoardRequest.content(), memberId
+		);
+	}
+}
