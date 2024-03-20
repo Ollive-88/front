86db396329097b874911d0c3c0922fd343d2d394
bufflect <bndgc@naver.com>
2024-03-20 21:41:38 +0900

    Add: BoardService

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/BoardService.java
@@ -0,0 +1,10 @@
+package org.palpalmans.ollive_back.domain.board.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+}
