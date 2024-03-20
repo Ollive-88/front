15afcf4e466bbe5afaf0bc6b3a847be2e72992ed
bufflect <bndgc@naver.com>
2024-03-20 23:36:25 +0900

    Add: WriteBoardRequest

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/model/dto/WriteBoardRequest.java
@@ -0,0 +1,18 @@
+package org.palpalmans.ollive_back.domain.board.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public record WriteBoardRequest(
+        @NotBlank
+        String title,
+        @NotBlank
+        String content,
+        List<String> tagNames,
+        @RequestPart(required = false)
+        List<MultipartFile> images
+) {
+}
