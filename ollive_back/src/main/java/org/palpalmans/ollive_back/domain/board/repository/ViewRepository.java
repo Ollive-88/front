2387bd173106c27c26df8619a5fd4ce21e6cc74e
bufflect <bndgc@naver.com>
2024-03-20 21:44:01 +0900

    Add: ViewRepository

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/repository/ViewRepository.java
@@ -0,0 +1,8 @@
+package org.palpalmans.ollive_back.domain.board.repository;
+
+
+import org.palpalmans.ollive_back.domain.board.model.entity.View;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ViewRepository extends JpaRepository<View, Long> {
+}
