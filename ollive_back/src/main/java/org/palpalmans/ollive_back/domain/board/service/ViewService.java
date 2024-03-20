32f0dd12e4a9188169bfcd7ebae52edd0a9a2881
bufflect <bndgc@naver.com>
2024-03-20 23:07:00 +0900

    Feat: ViewService-saveViews

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/ViewService.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/ViewService.java
@@ -1,11 +1,19 @@
 package org.palpalmans.ollive_back.domain.board.service;
 
-import lombok.RequiredArgsConstructor;
+import org.palpalmans.ollive_back.domain.board.model.entity.Board;
+import org.palpalmans.ollive_back.domain.board.model.entity.View;
 import org.palpalmans.ollive_back.domain.board.repository.ViewRepository;
+import org.palpalmans.ollive_back.domain.member.model.entity.Member;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ViewService {
-    private final ViewRepository viewRepository;
+	private final ViewRepository viewRepository;
+
+	public void saveView(Board board, Member member) {
+		viewRepository.save(new View(board, member)); // TODO 로그인 구현시 수정 예정
+	}
 }
