02759ff4a1ea5ac8a0d281beaab32ca03c3d81e2
JuHyeong <bndgc@naver.com>
2024-03-20 13:32:42 +0900

    feat: saveImageFile

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/image/service/ImageFileService.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/image/service/ImageFileService.java
@@ -2,8 +2,12 @@ package org.palpalmans.ollive_back.domain.image.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class ImageFileService {
+    public String saveImageFile(MultipartFile multipartFiles) {
+        return "savedPath";
+    }
 }
