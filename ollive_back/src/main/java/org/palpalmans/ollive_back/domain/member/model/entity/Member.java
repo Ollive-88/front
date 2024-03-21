ad919e5da3efc4ad2be5f74c480fed7aaae79ca1
Junhyun An <alloy0301@gmail.com>
2024-03-21 15:25:17 +0900

    Mod : Member Entity Build pattern, access Protected

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/entity/Member.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/entity/Member.java
@@ -1,15 +1,18 @@
 package org.palpalmans.ollive_back.domain.member.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.palpalmans.ollive_back.common.BaseTimeEntity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = PROTECTED)
 public class Member extends BaseTimeEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     private long id;
 
     @Column(nullable = false)
@@ -33,4 +36,16 @@ public class Member extends BaseTimeEntity {
 
     @Column(nullable = false)
     private String role;
+
+    @Builder
+    public Member(String email, String password, String gender, String birthday, String name, String nickname, String role){
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.name = name;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
 }
