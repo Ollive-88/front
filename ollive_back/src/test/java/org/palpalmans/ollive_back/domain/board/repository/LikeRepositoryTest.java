6ec43d87916b611a5c9414571deac9d327e47163
JuHyeong <bndgc@naver.com>
2024-03-21 14:04:47 +0900

    Test: LikeRepositoryTest

---
+++ ollive_back/src/test/java/org/palpalmans/ollive_back/domain/board/repository/LikeRepositoryTest.java
@@ -0,0 +1,49 @@
+package org.palpalmans.ollive_back.domain.board.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.palpalmans.ollive_back.domain.board.model.entity.Board;
+import org.palpalmans.ollive_back.domain.board.model.entity.Like;
+import org.palpalmans.ollive_back.domain.member.model.entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+class LikeRepositoryTest {
+    @Autowired
+    private LikeRepository likeRepository;
+
+    @Test
+    @DisplayName("Like Save Test")
+    @Transactional
+    void saveLike() {
+        Board board = new Board("title", "content", 1L);
+        Member member = Member.builder()
+                .name("name")
+                .nickname("nickname")
+                .email("email")
+                .birthday("birthday")
+                .gender("gender")
+                .password("password")
+                .role("role")
+                .build();
+
+        Like like = likeRepository.save(new Like(board, member));
+        List<Like> likes = likeRepository.findAll();
+        Like foundLike = likes.get(0);
+
+        assertThat(likes.size()).isEqualTo(1);
+        assertThat(foundLike).usingRecursiveAssertion().isEqualTo(like);
+    }
+}
