3984a11f1b8a08b7d8390bb9d5f260c35875508d
Junhyun An <alloy0301@gmail.com>
2024-03-22 13:33:19 +0900

    Add : INVALID_ROLE in JoinRequestStatus

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/status/JoinRequestStatus.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/status/JoinRequestStatus.java
@@ -1,5 +1,5 @@
 package org.palpalmans.ollive_back.domain.member.model.status;
 
 public enum JoinRequestStatus {
-    NULL_EXIST, EMAIL_DUPLICATED, JOIN_SUCCESS
+    NULL_EXIST, EMAIL_DUPLICATED, INVALID_ROLE, JOIN_SUCCESS
 }
