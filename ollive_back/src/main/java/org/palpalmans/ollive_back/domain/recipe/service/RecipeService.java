4ab76cb436889ee3a4ccb8a45e89e3257cd54284
doongdang <doongdang@naver.com>
2024-03-22 16:36:10 +0900

    Add       : RecipeService

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/service/RecipeService.java
@@ -0,0 +1,9 @@
+package org.palpalmans.ollive_back.domain.recipe.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RecipeService {
+}
