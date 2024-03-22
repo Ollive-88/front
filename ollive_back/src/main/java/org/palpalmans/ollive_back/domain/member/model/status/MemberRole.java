ad03fc1c24a68f940a308c51b10c4c88990b4212
Junhyun An <alloy0301@gmail.com>
2024-03-22 12:48:31 +0900

    Mod : Make MemberRole and SocialType to Enum

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/status/MemberRole.java
@@ -0,0 +1,7 @@
+package org.palpalmans.ollive_back.domain.member.model.status;
+
+public enum MemberRole {
+    ROLE_ADMIN,
+    ROLE_REGISTERED_MEMBER,
+    ROLE_NON_REGISTERED_MEMBER
+}
---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/status/SocialType.java
@@ -0,0 +1,6 @@
+package org.palpalmans.ollive_back.domain.member.model.status;
+
+public enum SocialType {
+    GOOGLE,
+    KAKAO
+}
