c0e9e55ecd9e704780f3679ec8c9e38763939c2d
Junhyun An <alloy0301@gmail.com>
2024-03-22 13:32:30 +0900

    Add : builder on Entities

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/entity/NormalMember.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/entity/NormalMember.java
@@ -3,13 +3,24 @@ package org.palpalmans.ollive_back.domain.member.model.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.palpalmans.ollive_back.domain.member.model.status.MemberRole;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NormalMember extends Member{
+
     @Column(nullable = false)
     private String password;
+
+    @Builder
+    public NormalMember(String email, String password, String gender, String birthday,
+                        String name, String nickname, MemberRole role, String profilepicture) {
+        super(email, gender, birthday, name, nickname, role, profilepicture);
+        this.password = password;
+    }
+
 }
--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/entity/SocialMember.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/model/entity/SocialMember.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.palpalmans.ollive_back.domain.member.model.status.MemberRole;
 import org.palpalmans.ollive_back.domain.member.model.status.SocialType;
 
 @Entity
@@ -16,4 +18,11 @@ public class SocialMember extends Member{
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private SocialType socialType;
+
+    @Builder
+    public SocialMember(String email, SocialType socialType, String gender, String birthday,
+                        String name, String nickname, MemberRole role, String profilepicture) {
+        super(email, gender, birthday, name, nickname, role, profilepicture);
+        this.socialType = socialType;
+    }
 }
