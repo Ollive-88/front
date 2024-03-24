31cf534581bbab053a22eb7d87233057b5f3e87b
SeokHyun Cho <doongdang@naver.com>
2024-03-25 00:55:28 +0900

    Feat      : RecipeDetail 조회

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/controller/RecipeController.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/controller/RecipeController.java
@@ -1,13 +1,26 @@
 package org.palpalmans.ollive_back.domain.recipe.controller;
 
-import lombok.RequiredArgsConstructor;
+import org.palpalmans.ollive_back.domain.recipe.model.dto.RecipeDetailDto;
 import org.palpalmans.ollive_back.domain.recipe.service.RecipeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/recipes")
+@Validated
 public class RecipeController {
     private final RecipeService recipeService;
 
-
+    @GetMapping("/{recipeId}")
+    public ResponseEntity<RecipeDetailDto> getRecipe(@PathVariable @Positive Long recipeId) {
+        return ResponseEntity.ok().body(recipeService.getRecipe(recipeId));
+    }
 }
--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/service/RecipeService.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/service/RecipeService.java
@@ -1,9 +1,38 @@
 package org.palpalmans.ollive_back.domain.recipe.service;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+import org.palpalmans.ollive_back.domain.recipe.model.dto.RecipeDetailDto;
+import org.palpalmans.ollive_back.domain.recipe.model.dto.RecipeDto;
+import org.palpalmans.ollive_back.domain.recipe.model.dto.RecipeIngredientDto;
+import org.palpalmans.ollive_back.domain.recipe.model.dto.RecipeMapper;
+import org.palpalmans.ollive_back.domain.recipe.model.dto.RecipeProcessDto;
+import org.palpalmans.ollive_back.domain.recipe.model.entity.Recipe;
+import org.palpalmans.ollive_back.domain.recipe.repository.RecipeRepository;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class RecipeService {
+    private final RecipeRepository recipeRepository;
+
+    public RecipeDetailDto getRecipe(Long recipeId) {
+        //FIXME RuntimeException 변경 필요
+        Recipe recipe = recipeRepository.findById(recipeId).orElseThrow(() -> new RuntimeException("존재하지 않는 레시피입니다."));
+        RecipeDto recipeDto = RecipeMapper.toRecipeDto(recipe);
+        List<RecipeIngredientDto> recipeIngredientDtos = recipe.getRecipeIngredients().stream()
+            .map(
+				RecipeMapper::toRecipeIngredientDto
+            )
+            .toList();
+
+        List<RecipeProcessDto> recipeProcessDtos = recipe.getRecipeProcesses().stream()
+            .map(
+                RecipeMapper::toRecipeProcessDto
+            ).toList();
+
+        return RecipeMapper.toRecipeDetailDto(recipeDto, recipeIngredientDtos, recipeProcessDtos);
+    }
 }
