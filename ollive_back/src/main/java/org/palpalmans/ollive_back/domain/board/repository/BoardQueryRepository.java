81c6ca47b8ccb323c6741f87d0ef2a32a1193323
JuHyeong <bndgc@naver.com>
2024-03-26 08:59:43 +0900

    Add       : BoardQueryRepository

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/repository/BoardQueryRepository.java
@@ -0,0 +1,13 @@
+package org.palpalmans.ollive_back.domain.board.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.palpalmans.ollive_back.domain.board.model.entity.QBoard;
+import org.palpalmans.ollive_back.domain.board.model.entity.QBoardTag;
+import org.palpalmans.ollive_back.domain.board.model.entity.QTag;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardQueryRepository {
+}
