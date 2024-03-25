6d617eae27657e6595843f26fd97361fb1cf7437
JuHyeong <bndgc@naver.com>
2024-03-26 08:33:14 +0900

    Mod       : toString

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/model/entity/Tag.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/model/entity/Tag.java
@@ -2,16 +2,12 @@ package org.palpalmans.ollive_back.domain.board.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.palpalmans.ollive_back.common.BaseTimeEntity;
 
-import static jakarta.persistence.FetchType.*;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
@@ -33,5 +29,13 @@ public class Tag extends BaseTimeEntity {
 
     public Tag(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
