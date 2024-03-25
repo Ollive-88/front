8691fcb1a6081e820dea2a873d45a23ca067450b
JuHyeong <bndgc@naver.com>
2024-03-26 08:49:55 +0900

    Mod       : countLikesByBoard

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/repository/LikeRepository.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/repository/LikeRepository.java
@@ -1,7 +1,9 @@
 package org.palpalmans.ollive_back.domain.board.repository;
 
+import org.palpalmans.ollive_back.domain.board.model.entity.Board;
 import org.palpalmans.ollive_back.domain.board.model.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+	int countLikesByBoard(Board board);
 }
