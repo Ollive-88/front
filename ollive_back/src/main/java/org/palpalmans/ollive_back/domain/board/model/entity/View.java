266762192d97423f2637a391f7b56de79eb97776
JuHyeong <bndgc@naver.com>
2024-03-26 08:40:48 +0900

    Style       : import 정리

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/model/entity/View.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/model/entity/View.java
@@ -1,46 +1,40 @@
 package org.palpalmans.ollive_back.domain.board.model.entity;
 
-import static jakarta.persistence.CascadeType.*;
-import static jakarta.persistence.FetchType.*;
-import static jakarta.persistence.GenerationType.*;
-import static lombok.AccessLevel.*;
-
-import org.palpalmans.ollive_back.domain.member.model.entity.Member;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.palpalmans.ollive_back.domain.member.model.entity.Member;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Table(name = "views")
 @Getter
 @Entity
 @NoArgsConstructor(access = PROTECTED)
 public class View {
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 
-	@ManyToOne(fetch = LAZY, cascade = PERSIST)
-	@JoinColumn(name = "board_id", nullable = false)
-	private Board board;
+    @ManyToOne(fetch = LAZY, cascade = PERSIST)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
 
-	@ManyToOne(fetch = LAZY, cascade = PERSIST)
-	@JoinColumn(name = "member_id", nullable = false)
-	private Member member;
+    @ManyToOne(fetch = LAZY, cascade = PERSIST)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
-	public View(Long id, Board board, Member member) {
-		this.id = id;
-		this.board = board;
-		this.member = member;
-	}
+    public View(Long id, Board board, Member member) {
+        this.id = id;
+        this.board = board;
+        this.member = member;
+    }
 
-	public View(Board board, Member member) {
-		this.board = board;
-		this.member = member;
-	}
+    public View(Board board, Member member) {
+        this.board = board;
+        this.member = member;
+    }
 }
