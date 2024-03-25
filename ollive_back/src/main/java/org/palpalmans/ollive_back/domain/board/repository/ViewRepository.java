bffafb68d0383611c3c4bcb312e138b0019033bc
JuHyeong <bndgc@naver.com>
2024-03-26 08:51:38 +0900

    Mod       : countViewsByBoard

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/repository/ViewRepository.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/repository/ViewRepository.java
@@ -1,8 +1,9 @@
 package org.palpalmans.ollive_back.domain.board.repository;
 
-
+import org.palpalmans.ollive_back.domain.board.model.entity.Board;
 import org.palpalmans.ollive_back.domain.board.model.entity.View;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ViewRepository extends JpaRepository<View, Long> {
+    int countViewsByBoard(Board board);
 }
