667f57306244d62422a4d9125ea06ce06ab3b808
JuHyeong <bndgc@naver.com>
2024-03-26 09:04:04 +0900

    Feat      : getLikeCount

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/LikeService.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/service/LikeService.java
@@ -1,21 +1,25 @@
 package org.palpalmans.ollive_back.domain.board.service;
 
+import lombok.RequiredArgsConstructor;
 import org.palpalmans.ollive_back.domain.board.model.entity.Board;
 import org.palpalmans.ollive_back.domain.board.model.entity.Like;
 import org.palpalmans.ollive_back.domain.board.repository.LikeRepository;
 import org.palpalmans.ollive_back.domain.member.model.entity.Member;
 import org.springframework.stereotype.Service;
-
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
-	private final LikeRepository likeRepository;
+    private final LikeRepository likeRepository;
 
-	@Transactional
-	public void saveLike(Board board, Member memeber) {
-		likeRepository.save(new Like(board, memeber));
-	}
+    @Transactional
+    public void saveLike(Board board, Member memeber) {
+        likeRepository.save(new Like(board, memeber));
+    }
+
+    @Transactional(readOnly = true)
+    public int getLikeCount(Board board) {
+        return likeRepository.countLikesByBoard(board);
+    }
 }
