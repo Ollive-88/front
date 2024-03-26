d404a7892170dd293b05cf4da2817439d08f0289
Junhyun An <alloy0301@gmail.com>
2024-03-26 11:59:13 +0900

    Add : LoginFilter

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/security/filter/LoginFilter.java
@@ -0,0 +1,97 @@
+package org.palpalmans.ollive_back.domain.member.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.palpalmans.ollive_back.domain.member.security.details.CustomMemberDetails;
+import org.palpalmans.ollive_back.domain.member.security.service.JwtService;
+import org.springframework.lang.Nullable;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+@Slf4j
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtService jwtService;
+
+    private static final String SPRING_SECURITY_FORM_EMAIL_KEY = "email";
+
+    @Nullable
+    protected String obtainEmail(HttpServletRequest request) {
+        //새로운 파라미터 생성
+        return request.getParameter(SPRING_SECURITY_FORM_EMAIL_KEY);
+    }
+
+    public LoginFilter(AuthenticationManager authenticationManager, JwtService jwtService){
+
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        //클라이언트 요청에서 memberLoginId, memberPassword 추출
+        String email = obtainEmail(request);
+        String password = obtainPassword(request);
+
+        log.info("LoginFilter.attemptAuthentication() email = {}", email);
+        log.info("LoginFilter.attemptAuthentication() password = {}", password);
+
+        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, password, null);
+        log.info("authToken = {}",authToken);
+        //token에 담은 검증을 위한 AuthenticationManager로 전달
+        //authenticationManager가 검증 해줍니다.
+        return authenticationManager.authenticate(authToken);
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+        CustomMemberDetails customMemberDetails = (CustomMemberDetails) authentication.getPrincipal();
+
+        long memberId = customMemberDetails.getMemberId();
+        String memberLoginId = customMemberDetails.getUsername();
+
+        //role 값 뽑아내기
+        log.info("LoginFilter.successfulAuthentication() authentication = {}", authentication);
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        log.info("LoginFilter.successfulAuthentication() authorities = {}", authorities.toString());
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+        log.info("role = {}", role);
+
+        //토큰생성 access+refresh
+//        GeneratedToken token = jwtUtil.generateToken(memberId, memberLoginId, role);
+//
+//        String accessToken = token.getAccessToken();
+//        String refreshToken = token.getRefreshToken();
+//        log.info("accessToken = {}", accessToken);
+//        log.info("refreshToken = {}", refreshToken);
+//
+//        response.addHeader("Authorization", "Bearer " + accessToken);
+//        response.addHeader("Refresh", "Bearer " + refreshToken);
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+        response.setStatus(409);
+    }
+
+}
