58f0a4b03a20dc819afb6eb99962921237c43323
doongdang <doongdang@naver.com>
2024-03-26 12:32:21 +0900

    Add       : RecipeSearchRequest

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/model/dto/request/RecipeSearchRequest.java
@@ -0,0 +1,20 @@
+package org.palpalmans.ollive_back.domain.recipe.model.dto.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record RecipeSearchRequest(
+        @NotNull
+        String recipeCase,
+        @NotNull
+        String recipeCategory,
+        @NotNull
+        Long lastRecipeId,
+        int size,
+        @NotEmpty
+        List<String> havingIngredients,
+        List<String> dislikeIngredients
+) {
+}
