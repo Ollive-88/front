03212e320b12f8a634347a7a10da545725a6f433
bufflect <bndgc@naver.com>
2024-03-26 23:34:51 +0900

    Add      : GlobalExceptionHandler

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,84 @@
+package org.palpalmans.ollive_back.common.exception;
+
+import static org.springframework.http.HttpStatus.*;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
+	public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(
+		HttpServletRequest request, BindException bindException
+	) {
+		printException(bindException);
+		List<FieldError> fieldErrors = bindException.getBindingResult().getFieldErrors();
+
+		StringBuilder errorMessage = new StringBuilder();
+		errorMessage.append("validation failed fields: [ ");
+
+		int size = fieldErrors.size();
+		for (int i = 0; i < size; i++) {
+			errorMessage.append(fieldErrors.get(i).getField());
+			if (i == size - 1) {
+				continue;
+			}
+			errorMessage.append(", ");
+		}
+		errorMessage.append(" ]");
+
+		return new ResponseEntity<>(
+			ErrorResponse.of(errorMessage.toString(), request.getRequestURI(), fieldErrors),
+			HttpStatus.BAD_REQUEST
+		);
+	}
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorResponse> NotFoundExceptionHandler(
+		HttpServletRequest request, Exception exception
+	) {
+		printException(exception);
+		return new ResponseEntity<>(ErrorResponse.of(
+			exception.getMessage(), request.getRequestURI()),
+			NOT_FOUND
+		);
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErrorResponse> runtimeExceptionHandler(
+		HttpServletRequest request, RuntimeException runtimeException
+	) {
+		printException(runtimeException);
+		return new ResponseEntity<>(ErrorResponse.of(
+			runtimeException.getMessage(), request.getRequestURI()),
+			INTERNAL_SERVER_ERROR
+		);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> exceptionHandler(
+		HttpServletRequest request, Exception exception
+	) {
+		printException(exception);
+		return new ResponseEntity<>(
+			ErrorResponse.of(exception.getMessage(), request.getRequestURI()),
+			INTERNAL_SERVER_ERROR
+		);
+	}
+
+	private void printException(Exception exception) {
+		log.error("exception 발생: ", exception);
+	}
+}
