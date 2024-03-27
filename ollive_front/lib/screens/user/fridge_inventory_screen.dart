505cc413afb1c15a446b5718d87dcfc47328d6a0
황준식 <wnstlr0908@naver.com>
2024-03-27 17:42:31 +0900

    Feat: 재료 추가 및 변경 기능 확정

--- ollive_front/lib/screens/user/fridge_inventory_screen.dart
+++ ollive_front/lib/screens/user/fridge_inventory_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:ollive_front/models/user/user_model.dart';
 
@@ -16,6 +17,8 @@ class FridgeInventoryScreen extends StatefulWidget {
 class _FridgeInventoryScreenState extends State<FridgeInventoryScreen> {
   List<UserIngredients> ingredients = userIngredientsList;
   DateTime today = DateTime.now();
+  final TextEditingController _ingredientController = TextEditingController();
+  var expirationDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -29,94 +32,20 @@ class _FridgeInventoryScreenState extends State<FridgeInventoryScreen> {
         width: 130,
         child: FloatingActionButton.large(
           onPressed: () {
-            showModalBottomSheet<void>(
-                backgroundColor: Colors.transparent,
-                isScrollControlled: true,
-                context: context,
-                builder: (BuildContext context) {
-                  return Padding(
-                    padding: EdgeInsets.only(
-                        bottom: MediaQuery.of(context).viewInsets.bottom),
-                    child: Container(
-                      // height: 350,
-                      decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(20))),
-                      child: Padding(
-                        padding: const EdgeInsets.all(40),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  '재료명',
-                                  style: TextStyle(fontSize: 17),
-                                ),
-                                SizedBox(
-                                  width: 200,
-                                  height: 60,
-                                  child: TextField(
-                                    cursorColor: Color(0xFF30AF98),
-                                    decoration: InputDecoration(
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(15)),
-                                      ),
-                                      focusedBorder: OutlineInputBorder(
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(15)),
-                                          borderSide: BorderSide(
-                                              color: Color(0xFF30AF98))),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            const Row(
-                              children: [
-                                Text(
-                                  '유통기한',
-                                  style: TextStyle(fontSize: 17),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 120,
-                              child: CupertinoDatePicker(
-                                mode: CupertinoDatePickerMode.date,
-                                minimumYear: DateTime.now().year,
-                                onDateTimeChanged: (DateTime date) {},
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                OutlinedButton(
-                                    onPressed: () {}, child: const Text('hi'))
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                });
+            _ingredientController.clear();
+            showBottomSheet(context, null);
           },
           foregroundColor: Colors.white,
           backgroundColor: const Color(0xFF30AF98),
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          child: const Text('재료 추가하기'),
+          child: const Text(
+            '재료 추가하기',
+            style: TextStyle(fontSize: 16),
+          ),
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: Column(
         children: [
           const SizedBox(
@@ -135,14 +64,15 @@ class _FridgeInventoryScreenState extends State<FridgeInventoryScreen> {
                     return Slidable(
                       key: Key(ingredient.ingredientName),
                       startActionPane: ActionPane(
-                        extentRatio: 0.4,
+                        extentRatio: 0.25,
                         motion: const BehindMotion(),
                         children: [
                           SlidableAction(
                             backgroundColor: const Color(0xFF30AF98),
-                            label: '유통기한 변경',
+                            label: '정보변경',
                             onPressed: (context) {
-                              showdialog(context, index);
+                              _updateText(ingredient.ingredientName);
+                              showBottomSheet(context, index);
                             },
                           ),
                         ],
@@ -170,6 +100,155 @@ class _FridgeInventoryScreenState extends State<FridgeInventoryScreen> {
     );
   }
 
+  // bottomSheet
+  Future<void> showBottomSheet(BuildContext context, int? index) {
+    var initDate = index != null
+        ? DateFormat('yyyy-MM-dd').parse(ingredients[index].expirationDate)
+        : DateTime.now();
+
+    return showModalBottomSheet<void>(
+        backgroundColor: Colors.transparent,
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext context) {
+          return Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(20))),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: const Icon(Icons.close_rounded),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            '재료명',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                          SizedBox(
+                            width: 200,
+                            height: 60,
+                            child: TextField(
+                              autofocus: true,
+                              controller: _ingredientController,
+                              cursorColor: const Color(0xFF30AF98),
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15)),
+                                    borderSide:
+                                        BorderSide(color: Color(0xFF30AF98))),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Row(
+                        children: [
+                          Text(
+                            '유통기한',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 120,
+                        child: CupertinoDatePicker(
+                          initialDateTime: initDate,
+                          mode: CupertinoDatePickerMode.date,
+                          minimumYear: initDate.year - 1,
+                          onDateTimeChanged: (DateTime date) {
+                            expirationDate = date;
+                          },
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_ingredientController.text.isEmpty) {
+                                FocusScope.of(context).unfocus();
+                                // 약간의 딜레이 후에 토스트 메시지를 띄운다.
+                                Future.delayed(
+                                    const Duration(milliseconds: 100), () {
+                                  showToast();
+                                });
+                              } else {
+                                if (index != null) {
+                                  setState(() {
+                                    ingredients[index].ingredientName =
+                                        _ingredientController.text;
+                                    ingredients[index].expirationDate =
+                                        DateFormat('yyyy-MM-dd')
+                                            .format(expirationDate);
+                                  });
+                                } else {
+                                  final newIngredient = UserIngredients(
+                                      ingredientName:
+                                          _ingredientController.text,
+                                      expirationDate: DateFormat('yyyy-MM-dd')
+                                          .format(expirationDate));
+                                  userIngredientsList.add(newIngredient);
+
+                                  setState(() {});
+                                }
+                                // 입력 필드 초기화
+                                _ingredientController.clear();
+                                expirationDate = DateTime.now();
+                                Navigator.of(context).pop();
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFF30AF98),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15))),
+                            child: Text(
+                              index != null ? '변경' : '추가',
+                              style: const TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
   void _onDismissed(int index, Actions action) {
     setState(() => ingredients.removeAt(index));
 
@@ -179,6 +258,18 @@ class _FridgeInventoryScreenState extends State<FridgeInventoryScreen> {
       case Actions.change:
         _showSnackBar(context, '변경되었습니다.', Colors.grey);
     }
+  }
+
+  void showToast() {
+    Fluttertoast.showToast(
+      msg: '재료명을 입력해주세요.',
+    );
+  }
+
+  void _updateText(txt) {
+    setState(() {
+      _ingredientController.text = txt;
+    });
   }
 
   void _showSnackBar(BuildContext context, String msg, Color color) {
@@ -202,55 +293,6 @@ class _FridgeInventoryScreenState extends State<FridgeInventoryScreen> {
     } else {
       return '${difference.inDays}일 지남';
     }
-  }
-
-  Future<dynamic> showdialog(BuildContext context, int index) {
-    var initDate =
-        DateFormat('yyyy-MM-dd').parse(ingredients[index].expirationDate);
-
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) => AlertDialog(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        backgroundColor: Colors.white,
-        title: const Text(
-          '유통기한 설정',
-          style: TextStyle(fontSize: 18),
-        ),
-        content: SizedBox(
-          height: 120,
-          child: CupertinoDatePicker(
-            initialDateTime: initDate,
-            mode: CupertinoDatePickerMode.date,
-            minimumYear: initDate.year,
-            onDateTimeChanged: (DateTime date) {
-              initDate = date;
-            },
-          ),
-        ),
-        actions: [
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text(
-                '취소',
-                style: TextStyle(color: Colors.black54),
-              )),
-          TextButton(
-              onPressed: () {
-                setState(() {
-                  ingredients[index].expirationDate =
-                      DateFormat('yyyy-MM-dd').format(initDate);
-                });
-                Navigator.of(context).pop();
-              },
-              child: const Text('변경',
-                  style: TextStyle(
-                      color: Color(0xFF30AF98), fontWeight: FontWeight.w600))),
-        ],
-      ),
-    );
   }
 
   Widget buildIngredientListTitle(UserIngredients ingredient) => Builder(
