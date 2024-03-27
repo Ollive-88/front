3cbb8164f059e51dc2da1f12fabda1e47ed7515e
JuHyeong <bndgc@naver.com>
2024-03-27 20:03:43 +0900

    Feat      : BoardRepository
    - `findByIdWithComments(boardId)`
      - fetch join : `boardId`에 해당하는 board를 `comments` 와 함께 조회하는 메서드
      - left join : `comments` 가 없을 시 boardId에 해당하는 board만 조회

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/repository/BoardRepository.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/repository/BoardRepository.java
@@ -2,6 +2,11 @@ package org.palpalmans.ollive_back.domain.board.repository;
 
 import org.palpalmans.ollive_back.domain.board.model.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query("select b from Board b left join fetch b.comments where b.id=:boardId")
+    Optional<Board> findByIdWithComments(Long boardId);
 }
