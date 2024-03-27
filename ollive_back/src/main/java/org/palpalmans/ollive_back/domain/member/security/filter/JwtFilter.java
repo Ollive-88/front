3c81f6463c46b5e0dd73a4075f1d2ce4c6380fd1
Junhyun An <alloy0301@gmail.com>
2024-03-27 09:35:02 +0900

    Add : JwtFilter

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/security/filter/JwtFilter.java
@@ -0,0 +1,91 @@
+package org.palpalmans.ollive_back.domain.member.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.palpalmans.ollive_back.domain.member.model.entity.Member;
+import org.palpalmans.ollive_back.domain.member.model.entity.NormalMember;
+import org.palpalmans.ollive_back.domain.member.model.status.MemberRole;
+import org.palpalmans.ollive_back.domain.member.security.details.CustomMemberDetails;
+import org.palpalmans.ollive_back.domain.member.security.service.JwtService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String atc= request.getHeader("Authorization");
+        log.info("atc is {}", atc);
+
+        if (!StringUtils.hasText(atc)) {
+            log.info("Authorization head is blank");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (!atc.startsWith("Bearer ")) {
+
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        log.info("atc = {} ", atc);
+
+        String token = atc.split(" ")[1];
+
+        log.info("token = {} ", token);
+
+        if (jwtService.isExpired(token)) {
+            log.info("access token expired");
+
+            if(request.getHeader("Refresh") != null){
+
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
+            return;
+
+        }
+
+        String email = jwtService.getEmail(token);
+        MemberRole role = jwtService.getRole(token);
+
+        Member member = Member.builder()
+                .email(email)
+                .role(role)
+                .build();
+
+        log.info("member.email = {}", member.getEmail());
+        log.info("member.role ={}", member.getRole());
+
+
+        CustomMemberDetails customMemberDetails = new CustomMemberDetails((NormalMember) member);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(customMemberDetails, null, customMemberDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        filterChain.doFilter(request, response);
+
+    }
+}
+
