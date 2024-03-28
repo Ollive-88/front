5441c9376d12a395534f9b08a6f25c6481c02181
JuHyeong <bndgc@naver.com>
2024-03-28 12:20:06 +0900

    Mod       : BoardMapper
    - `toGetBoardDetailResponse`

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/model/BoardMapper.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/board/model/BoardMapper.java
@@ -1,8 +1,16 @@
 package org.palpalmans.ollive_back.domain.board.model;
 
 import org.palpalmans.ollive_back.domain.board.model.dto.request.WriteBoardRequest;
+import org.palpalmans.ollive_back.domain.board.model.dto.response.GetBoardDetailResponse;
 import org.palpalmans.ollive_back.domain.board.model.dto.response.GetBoardResponse;
+import org.palpalmans.ollive_back.domain.board.model.dto.response.GetCommentResponse;
 import org.palpalmans.ollive_back.domain.board.model.entity.Board;
+import org.palpalmans.ollive_back.domain.member.model.entity.Member;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.palpalmans.ollive_back.domain.board.model.CommentMapper.toGetCommentResponse;
 
 public class BoardMapper {
     public static Board toBoard(WriteBoardRequest writeBoardRequest, Long memberId) {
@@ -31,5 +39,24 @@ public class BoardMapper {
                                         .getName())
                         .toList())
                 .build();
+    }
+
+    public static GetBoardDetailResponse toGetBoardDetailResponse(
+            Board board, Member member,
+            int viewCount, int likeCount, List<String> images
+    ) {
+        return GetBoardDetailResponse.builder()
+                .title(board.getTitle())
+                .content(board.getContent())
+                .createdAt(board.getCreatedAt())
+                .viewCount(viewCount)
+                .likeCount(likeCount)
+                .images(images)
+                .comments(board.getComments()
+                        .stream()
+                        .map(comment -> toGetCommentResponse(comment, member))
+                        .sorted(Comparator.comparing(GetCommentResponse::createdAt))
+                        .toList()
+                ).build();
     }
 }
