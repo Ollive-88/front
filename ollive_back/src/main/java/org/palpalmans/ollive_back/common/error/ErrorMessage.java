44936ed3ee9cfbc5c2a04c1145087de185d6fced
JuHyeong <bndgc@naver.com>
2024-03-28 12:21:10 +0900

    Add       : ErrorMessage

---
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/common/error/ErrorMessage.java
@@ -0,0 +1,14 @@
+package org.palpalmans.ollive_back.common.error;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorMessage {
+    BOARD_NOT_FOUND("해당하는 게시글이 존재하지 않습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+}
