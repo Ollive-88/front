9839b91994be5aac4d24d6e2973b56dffb82913f
황준식 <wnstlr0908@naver.com>
2024-03-28 17:48:47 +0900

    Feat: 앨범에서 이미지 파일 가져오기 구현

--- ollive_front/lib/screens/user/setting/update_profile_image_screen.dart
+++ ollive_front/lib/screens/user/setting/update_profile_image_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileImageScreen extends StatefulWidget {
   const ProfileImageScreen({super.key});
@@ -20,12 +22,22 @@ class _ProfileImageScreenState extends State<ProfileImageScreen> {
     ),
   );
 
+  // 이미지 피커
+  final ImagePicker _picker = ImagePicker();
+  XFile pickedImg = XFile('');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('사진 편집'),
         centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('완료'),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -54,7 +66,9 @@ class _ProfileImageScreenState extends State<ProfileImageScreen> {
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      showdialog(context);
+                    },
                     style: buttonStyle,
                     child: const Text('사진 편집'),
                   ),
@@ -65,5 +79,104 @@ class _ProfileImageScreenState extends State<ProfileImageScreen> {
         ),
       ),
     );
+  }
+
+  Future<dynamic> showdialog(
+    BuildContext context,
+  ) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        backgroundColor: Colors.white,
+        title: const Text(
+          '프로필 사진',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        content: SizedBox(
+          height: 100,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        _pickImg();
+                      },
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.grey,
+                        alignment: Alignment.centerLeft,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius
+                              .zero, // 테두리를 없애기 위해 BorderRadius.zero로 설정
+                        ),
+                      ),
+                      child: const Text(
+                        '앨범에서 사진/동영상 선택',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.grey,
+                        alignment: Alignment.centerLeft,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius
+                              .zero, // 테두리를 없애기 위해 BorderRadius.zero로 설정
+                        ),
+                      ),
+                      child: const Text(
+                        '기본 이미지로 변경',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 이미지 선택 메서드
+  Future<void> _pickImg() async {
+    // 이미지(XFile)을 받아오고 상태갱신
+    XFile? image = await _picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 30,
+    );
+
+    if (image != null) {
+      setState(() {
+        pickedImg = image;
+      });
+    }
+  }
+
+  // 이미지 지우는 메서드
+  void deleteImage(int index) {
+    // _pickedImg.removeAt(index);
+    setState(() {});
+  }
+
+  // 파일 변환
+  static Future<XFile> getImageXFileByUrl(String url) async {
+    var file = await DefaultCacheManager().getSingleFile(url);
+    return XFile(file.path);
   }
 }
