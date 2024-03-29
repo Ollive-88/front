175eab10695bcd8c019165311dc2485e9c174d1c
황준식 <wnstlr0908@naver.com>
2024-03-29 18:01:23 +0900

    Feat: 로그인시 로컬 스토리 저장 로직 생성

---
+++ ollive_front/lib/provider/provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:ollive_front/main.dart';
+import 'package:ollive_front/models/user/user_model.dart';
+import 'package:ollive_front/service/user/user_service.dart';
+
+// 인스턴스 생성
+const secureStorage = FlutterSecureStorage();
+
+class UserController {
+  final mContext = navigatorKey.currentContext;
+
+  Future<void> login(Login input) async {
+    ResponseDTO responseDTO = await UserService().loginAction(input);
+    if (responseDTO.code == 1) {
+      // 토큰을 휴대폰에 저장
+      await secureStorage.write(key: 'token', value: responseDTO.token);
+      await secureStorage.write(
+          key: 'refreshToken', value: responseDTO.refreshToken);
+      // 게시판으로 이동
+      Navigator.popAndPushNamed(mContext!, '/home');
+    }
+  }
+}
