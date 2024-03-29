49f3cdd1c04ff2fdfbf79fbfcc33a00d2e54eff9
doongdang <doongdang@naver.com>
2024-03-29 17:42:53 +0900

    Feat      : 레시피 아이디, 회원 아이디 조회

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/repository/RecipeScoreRepository.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/repository/RecipeScoreRepository.java
@@ -3,8 +3,11 @@ package org.palpalmans.ollive_back.domain.recipe.repository;
 import org.palpalmans.ollive_back.domain.recipe.model.entity.RecipeScore;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RecipeScoreRepository extends JpaRepository<RecipeScore, Long>{
     Optional<RecipeScore> findByMemberIdAndRecipeId(Long memberId, Long recipeId);
+    List<RecipeScore> findByRecipeId(Long recipeId);
+    List<RecipeScore> findByMemberId(Long memberId);
 }
