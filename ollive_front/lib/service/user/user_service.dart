fca13a71c1e651223c7eaaf97c98fe4339692efd
황준식 <wnstlr0908@naver.com>
2024-03-29 18:04:31 +0900

    로그인 요청 응답 parsing 해서 값 전달

--- ollive_front/lib/service/user/user_service.dart
+++ ollive_front/lib/service/user/user_service.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:ollive_front/models/user/user_model.dart';
 
 final dio = Dio(BaseOptions(
-  baseUrl: "http://j10a508.p.ssafy.io:8080", // 위에서 실행시킨 스프링서버
+  baseUrl: "http://j10a508.p.ssafy.io", // 위에서 실행시킨 스프링서버
   contentType: "application/json; charset=utf-8",
 ));
 
@@ -14,14 +14,45 @@ class UserService {
       'name': input.name,
       'birthday': input.birthday,
       'gender': input.gender,
-      'role': input.role,
     };
+
     try {
       Response response = await dio.post('/join', data: requestBody);
-      print('Response status: ${response.statusCode}');
-      print('Response data: ${response.data}');
+      print('회원가입 성공! ${response.data}');
     } catch (e) {
       print('Error: $e');
+    }
+  }
+
+  Future<ResponseDTO> loginAction(Login input) async {
+    FormData formData = FormData.fromMap({
+      'email': input.email,
+      'password': input.password,
+    });
+
+    try {
+      Response response = await dio.post('/login', data: formData);
+
+      ResponseDTO responseDTO = ResponseDTO.fromJson();
+      // 토큰 받기
+      final authorization = response.headers['Authorization'];
+      final cookies = response.headers['Set-Cookie'];
+      if (cookies != null) {
+        final cookie = cookies.first;
+        // 쿠키 문자열에서 리프레시 토큰 값을 파싱
+        RegExp regExp = RegExp(r'Refresh=([^;]+)');
+        Match? match = regExp.firstMatch(cookie);
+        if (match != null) {
+          String refreshToken = match.group(1)!;
+          responseDTO.refreshToken = refreshToken;
+        }
+      }
+      if (authorization != null) {
+        responseDTO.token = authorization.first;
+      }
+      return responseDTO;
+    } catch (e) {
+      return ResponseDTO(code: -1, msg: "유저네임 혹은 비번이 틀렸습니다");
     }
   }
 }
