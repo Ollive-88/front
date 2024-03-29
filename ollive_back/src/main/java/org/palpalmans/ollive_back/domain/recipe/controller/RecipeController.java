26643ea49d71a0ee2e5f62a2ee14f9f16a282fb7
doongdang <doongdang@naver.com>
2024-03-29 17:43:47 +0900

    Feat      : 레시피 추천

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/controller/RecipeController.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/recipe/controller/RecipeController.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.palpalmans.ollive_back.common.security.details.CustomMemberDetails;
 import org.palpalmans.ollive_back.domain.recipe.model.dto.RecipeDto;
 import org.palpalmans.ollive_back.domain.recipe.model.dto.RecipeSummaryDto;
+import org.palpalmans.ollive_back.domain.recipe.model.dto.request.RecipeRecommendRequest;
 import org.palpalmans.ollive_back.domain.recipe.model.dto.request.RecipeScoreRequest;
 import org.palpalmans.ollive_back.domain.recipe.model.dto.request.RecipeSearchRequest;
+import org.palpalmans.ollive_back.domain.recipe.service.RecipeRecommendService;
 import org.palpalmans.ollive_back.domain.recipe.service.RecipeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,6 +23,7 @@ import java.util.List;
 @Validated
 public class RecipeController {
     private final RecipeService recipeService;
+    private final RecipeRecommendService recipeRecommendService;
 
     @GetMapping("/{recipeId}")
     public ResponseEntity<RecipeDto> getRecipe(@PathVariable Long recipeId) {
@@ -39,5 +42,16 @@ public class RecipeController {
     ) {
         Long memberId = customMemberDetails.getId();
         return ResponseEntity.ok().body(recipeService.generateOrUpdateScore(memberId, recipeScoreRequest));
+    }
+
+    @PostMapping("/recommendations")
+    public ResponseEntity<?> recommendRecipes(@Valid @RequestBody RecipeRecommendRequest request){
+        List<RecipeSummaryDto> recommendCandidates = recipeRecommendService.getRecommendCandidates(request);
+
+        if (recommendCandidates.size() <= 10){
+            return ResponseEntity.ok().body(recommendCandidates);
+        }else{
+            return ResponseEntity.ok().body(recommendCandidates.subList(0, 10));
+        }
     }
 }
