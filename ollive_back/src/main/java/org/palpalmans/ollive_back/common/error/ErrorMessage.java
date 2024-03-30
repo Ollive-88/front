c98b82054028f652f5c7ef5f8df4c2fa460547c7
JuHyeong <bndgc@naver.com>
2024-03-30 17:05:46 +0900

    Mod       : ErrorMessage
    - Image 관련 error message 추가

--- ollive_back/src/main/java/org/palpalmans/ollive_back/common/error/ErrorMessage.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/common/error/ErrorMessage.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorMessage {
+    NOT_BLANK("공백일 수 없습니다."),
     BOARD_NOT_FOUND("해당하는 게시글이 존재하지 않습니다."),
-    NOT_BLANK("공백일 수 없습니다.");
+    IMAGE_FILE_NOT_SAVED("이미지 파일이 저장되지 않았습니다."),
+    IMAGE_ENTITY_NOT_SAVED("이미지 데이터가 저장되지 않았습니다."),
+    UNKNOWN("알 수 없는 에러가 발생하였습니다.");
 
     private final String message;
 
