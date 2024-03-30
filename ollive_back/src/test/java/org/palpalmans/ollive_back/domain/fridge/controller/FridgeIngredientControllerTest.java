e4bb75caf34c139bfb9955f7554860c1e0044895
doongdang <whtjrgus0428@gmail.com>
2024-03-30 13:03:42 +0900

    Test      : 냉장고 재료 등록 테스트

---
+++ ollive_back/src/test/java/org/palpalmans/ollive_back/domain/fridge/controller/FridgeIngredientControllerTest.java
@@ -0,0 +1,91 @@
+package org.palpalmans.ollive_back.domain.fridge.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.palpalmans.ollive_back.domain.fridge.model.dto.FridgeIngredientRequest;
+import org.palpalmans.ollive_back.domain.member.model.entity.Member;
+import org.palpalmans.ollive_back.domain.member.model.entity.NormalMember;
+import org.palpalmans.ollive_back.domain.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Date;
+
+import static org.palpalmans.ollive_back.domain.member.model.status.MemberRole.ROLE_ADMIN;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class FridgeIngredientControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    Member member;
+    NormalMember normalMember;
+
+    private String objectToJson(Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(object);
+    }
+
+    private FridgeIngredientRequest request(){
+        return new FridgeIngredientRequest("감자", "2022-03-10");
+    }
+
+    @BeforeEach
+    void setUp() {
+        member = Member.builder()
+                .email("test@naver.com")
+                .name("name")
+                .nickname("nickname")
+                .birthday(new Date())
+                .profilePicture("profilePicture")
+                .role(ROLE_ADMIN)
+                .gender("male")
+                .build();
+
+        normalMember = new NormalMember(member, "1234");
+        memberRepository.save(normalMember);
+    }
+
+    @AfterEach
+    void tearDown() {
+        memberRepository.deleteAll();
+    }
+
+    @DisplayName("냉장고 재료 저장 테스트")
+    @Test
+    @WithUserDetails(value = "test@naver.com",
+            userDetailsServiceBeanName = "customMemberDetailsService",
+            setupBefore = TestExecutionEvent.TEST_EXECUTION
+    )
+    void registerFridgeIngredientTest() throws Exception {
+        //given
+        FridgeIngredientRequest request = request();
+
+        //when
+        mockMvc.perform(post("/fridge").contentType(APPLICATION_JSON).content(objectToJson(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"))
+                .andDo(print());
+        //then
+    }
+}
