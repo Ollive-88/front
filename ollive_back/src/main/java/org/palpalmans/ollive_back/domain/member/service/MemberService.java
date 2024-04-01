56e0412a231550cc332ee879e8eb2c66a5707575
Junhyun An <alloy0301@gmail.com>
2024-04-01 13:15:58 +0900

    Feat : load profile pic when get member info

--- ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/service/MemberService.java
+++ ollive_back/src/main/java/org/palpalmans/ollive_back/domain/member/service/MemberService.java
@@ -1,13 +1,18 @@
 package org.palpalmans.ollive_back.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.palpalmans.ollive_back.domain.image.model.ImageType;
+import org.palpalmans.ollive_back.domain.image.model.dto.GetImageResponse;
+import org.palpalmans.ollive_back.domain.image.service.ImageService;
 import org.palpalmans.ollive_back.domain.member.model.dto.response.MemberInfoResponse;
 import org.palpalmans.ollive_back.domain.member.model.entity.Member;
 import org.palpalmans.ollive_back.domain.member.model.entity.NormalMember;
 import org.palpalmans.ollive_back.domain.member.model.entity.SocialMember;
 import org.palpalmans.ollive_back.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -16,20 +21,24 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final ImageService imageService;
 
     public MemberInfoResponse getMemberInfo(long id){
 
         Member member = memberRepository.getMemberById(id)
                 .orElseThrow(() -> new NoSuchElementException("해당 ID를 가진 멤버가 존재하지 않습니다: " + id));
 
-        MemberInfoResponse memberInfoResponse = new MemberInfoResponse();
+        List<GetImageResponse> images = imageService.getImages(ImageType.PROFILE_PICTURE, id);
+        String profilePicture = images.isEmpty() ? "" : images.get(0).address();
 
-        memberInfoResponse.setProfilePicture(member.getProfilePicture());
+        MemberInfoResponse memberInfoResponse = new MemberInfoResponse();
         memberInfoResponse.setNickname(member.getNickname());
         memberInfoResponse.setName(member.getName());
         memberInfoResponse.setGender(member.getGender());
         memberInfoResponse.setEmail(member.getEmail());
         memberInfoResponse.setBirthday(member.getBirthday());
+        memberInfoResponse.setProfilePicture(profilePicture);
+
 
         return memberInfoResponse;
     }
@@ -43,10 +52,6 @@ public class MemberService {
     //이메일로 패스워드 포함 멤버 불러오기
     public Optional<NormalMember> getNormalMemberByEmail(String email){
         return  memberRepository.getNormalMemberByEmail(email);
-    }
-
-    public Optional<NormalMember> getNormalMemberById(long id){
-        return memberRepository.getNormalMemberById(id);
     }
 
     public Optional<SocialMember> getSocialMemberByEmail(String email){
@@ -81,7 +86,7 @@ public class MemberService {
     public Boolean modifyProfilePicture(long id, String profilePicture){
 
         Optional<Member> member = memberRepository.getMemberById(id);
-
+        //todo : profilepicture 새로운거 save
         if(member.isPresent()){
             Member now = member.get();
             now.changeProfilePicture(profilePicture);
@@ -103,5 +108,7 @@ public class MemberService {
         }
         return false;
     }
+
+
 
 }
