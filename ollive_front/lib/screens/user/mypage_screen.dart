a2ad6cee60d50c76b014edb4f134763b55d1b2d6
황준식 <wnstlr0908@naver.com>
2024-04-01 18:02:40 +0900

    Mod: 마이페이지 내 정보 조회로 변경

--- ollive_front/lib/screens/user/mypage_screen.dart
+++ ollive_front/lib/screens/user/mypage_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:ollive_front/models/user/user_simple_model.dart';
 import 'package:ollive_front/screens/user/fridge_inventory_screen.dart';
 import 'package:ollive_front/screens/user/hate_inventory_screen.dart';
 import 'package:ollive_front/screens/user/setting/setting_screen.dart';
+import 'package:ollive_front/service/user/user_service.dart';
 
-class MyPageScreen extends StatelessWidget {
-  MyPageScreen({super.key});
+class MyPageScreen extends StatefulWidget {
+  const MyPageScreen({super.key});
 
   static String baseAddress = 'assets/image/icons/';
+
+  @override
+  State<MyPageScreen> createState() => _MyPageScreenState();
+}
+
+class _MyPageScreenState extends State<MyPageScreen> {
   final List<List<dynamic>> settings = [
-    ['alramIcon.png', '알림'],
     ['refrigeratorIcon.png', '냉장고 관리'],
     ['ingredientsIcon.png', '싫어하는 재료 목록'],
-    ['recipeIcon.png', '즐겨 찾는 레시피'],
     ['clothesIcon.png', '최근 본 의류'],
     ['myBoardIcon.png', '내 게시글'],
     ['settingIcon.png', '설정'],
@@ -19,83 +25,109 @@ class MyPageScreen extends StatelessWidget {
 
   final List<Widget> pages = [
     const FridgeInventoryScreen(),
-    const FridgeInventoryScreen(),
     const HateInventoryScreen(),
-    const FridgeInventoryScreen(),
     const FridgeInventoryScreen(),
     const FridgeInventoryScreen(),
     SettingScreen(),
   ];
 
+  late Future<UserSimpleModel> userInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    userInfo = UserService.getUserInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 8.0,
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const Center(
+      body: FutureBuilder(
+        future: userInfo,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                ),
+                child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      MakeCircleImage(
-                        imageUrl:
-                            'https://i.ytimg.com/vi/gpFSXXhonVk/maxresdefault.jpg',
-                        width: 200,
-                        height: 200,
+                      Center(
+                        child: Column(
+                          children: [
+                            (snapshot.data?.imgUrl != null)
+                                ? MakeCircleImage(
+                                    imageUrl: snapshot.data!.imgUrl,
+                                    width: 200,
+                                    height: 200,
+                                  )
+                                : ClipOval(
+                                    child: Image.asset(
+                                      'assets/image/icons/basic_profile_img.png',
+                                      width: 200,
+                                      height: 200,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              snapshot.data!.nickname,
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w700),
+                            ),
+                          ],
+                        ),
                       ),
-                      SizedBox(
-                        height: 5,
+                      const SizedBox(
+                        height: 15,
                       ),
-                      Text(
-                        '난 밤이야 슈밤',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700),
+                      const Divider(
+                        thickness: 5,
+                        color: Color(0xFFEBEBE9),
                       ),
+                      for (var i = 0; i < 5; i++)
+                        Column(
+                          children: [
+                            ListTile(
+                              leading: Image.asset(
+                                '${MyPageScreen.baseAddress}${settings[i][0]}',
+                                width: 28,
+                                height: 28,
+                              ),
+                              title: Text(settings[i][1]),
+                              trailing:
+                                  const Icon(Icons.arrow_forward_ios_rounded),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => pages[i]));
+                              },
+                            ),
+                            // 마지막에는 생성 X
+                            i != 4
+                                ? const Divider(
+                                    height: 2,
+                                    color: Color(0xFFEEEEEC),
+                                  )
+                                : const SizedBox()
+                          ],
+                        )
                     ],
                   ),
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Divider(
-                  thickness: 5,
-                  color: Color(0xFFEBEBE9),
-                ),
-                for (var i = 0; i < 7; i++)
-                  Column(
-                    children: [
-                      ListTile(
-                        leading: Image.asset(
-                          '$baseAddress${settings[i][0]}',
-                          width: 28,
-                          height: 28,
-                        ),
-                        title: Text(settings[i][1]),
-                        trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => pages[i]));
-                        },
-                      ),
-                      // 마지막에는 생성 X
-                      i != 6
-                          ? const Divider(
-                              height: 2,
-                              color: Color(0xFFEEEEEC),
-                            )
-                          : const SizedBox()
-                    ],
-                  )
-              ],
-            ),
-          ),
-        ),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
