e095823802aa6ce56c8baf50514876d16d8b18ee
bufflect <bndgc@naver.com>
2024-04-02 21:12:04 +0900

    Mod      : init.sql

--- ollive_back/src/test/resources/sql/init.sql
+++ ollive_back/src/test/resources/sql/init.sql
@@ -14,21 +14,22 @@ DROP TABLE IF EXISTS `board`;
 DROP TABLE IF EXISTS `normal_member`;
 DROP TABLE IF EXISTS `social_member`;
 DROP TABLE IF EXISTS `member`;
+DROP TABLE IF EXISTS `cloth_member`;
 
 
 -- member
 CREATE TABLE member
 (
     id              BIGINT AUTO_INCREMENT PRIMARY KEY,
-    birthday        DATETIME(6) NOT NULL,
-    email           VARCHAR(255) NOT NULL,
-    gender          VARCHAR(255) NOT NULL,
-    name            VARCHAR(255) NOT NULL,
-    nickname        VARCHAR(255)          DEFAULT 'DEFAULTUSERNICKNAME' NOT NULL,
-    profile_picture VARCHAR(255) NULL,
+    birthday        DATETIME(6)                                                                 NOT NULL,
+    email           VARCHAR(255)                                                                NOT NULL,
+    gender          VARCHAR(255)                                                                NOT NULL,
+    name            VARCHAR(255)                                                                NOT NULL,
+    nickname        VARCHAR(255)                                                                         DEFAULT 'DEFAULTUSERNICKNAME' NOT NULL,
+    profile_picture VARCHAR(255)                                                                NULL,
     role            ENUM ('ROLE_ADMIN', 'ROLE_REGISTERED_MEMBER', 'ROLE_NON_REGISTERED_MEMBER') NOT NULL,
-    created_at      DATETIME     NOT NULL DEFAULT NOW(),
-    updated_at      DATETIME     NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_at      DATETIME                                                                    NOT NULL DEFAULT NOW(),
+    updated_at      DATETIME                                                                    NOT NULL DEFAULT NOW() ON UPDATE NOW(),
     CONSTRAINT UNIQUE (email)
 );
 
@@ -36,10 +37,10 @@ CREATE TABLE member
 -- social member
 CREATE TABLE social_member
 (
-    id          BIGINT   NOT NULL PRIMARY KEY,
+    id          BIGINT                   NOT NULL PRIMARY KEY,
     social_type ENUM ('GOOGLE', 'KAKAO') NOT NULL,
-    created_at  DATETIME NOT NULL DEFAULT NOW(),
-    updated_at  DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_at  DATETIME                 NOT NULL DEFAULT NOW(),
+    updated_at  DATETIME                 NOT NULL DEFAULT NOW() ON UPDATE NOW(),
     CONSTRAINT FK_SOCIAL_MEMBER_MEMBER_ID FOREIGN KEY (id) REFERENCES member (id) ON DELETE CASCADE
 );
 
@@ -74,9 +75,9 @@ CREATE TABLE board
 CREATE TABLE comments
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    board_id   BIGINT NULL,
-    member_id  BIGINT NULL,
-    content    TEXT NULL,
+    board_id   BIGINT   NULL,
+    member_id  BIGINT   NULL,
+    content    TEXT     NULL,
     created_at DATETIME NOT NULL DEFAULT NOW(),
     updated_at DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
     CONSTRAINT FK_COMMENTS_BOARD_ID FOREIGN KEY (board_id) REFERENCES board (id) ON DELETE CASCADE,
@@ -124,8 +125,8 @@ CREATE TABLE tag
 CREATE TABLE board_tag
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    tag_id     BIGINT NULL,
-    board_id   BIGINT NULL,
+    tag_id     BIGINT   NULL,
+    board_id   BIGINT   NULL,
     created_at DATETIME NOT NULL DEFAULT NOW(),
     updated_at DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
     CONSTRAINT FK_BOARD_TAG_ID FOREIGN KEY (tag_id) REFERENCES tag (id),
@@ -136,11 +137,11 @@ CREATE TABLE board_tag
 CREATE TABLE image
 (
     id           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    reference_id BIGINT   NOT NULL,
-    address      VARCHAR(255) NULL,
+    reference_id BIGINT                           NOT NULL,
+    address      VARCHAR(255)                     NULL,
     image_type   ENUM ('BOARD','PROFILE_PICTURE') NULL,
-    created_at   DATETIME NOT NULL DEFAULT NOW(),
-    updated_at   DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW()
+    created_at   DATETIME                         NOT NULL DEFAULT NOW(),
+    updated_at   DATETIME                         NOT NULL DEFAULT NOW() ON UPDATE NOW()
 );
 
 -- ###################
@@ -162,7 +163,7 @@ CREATE TABLE fridge_ingredient
     id        BIGINT AUTO_INCREMENT
         PRIMARY KEY,
     member_id BIGINT      NOT NULL,
-    end_at    DATE   NOT NULL,
+    end_at    DATE        NOT NULL,
     name      VARCHAR(50) NOT NULL,
     CONSTRAINT FK_FRIDGE_INGREDIENT_MEMBER
         FOREIGN KEY (member_id) REFERENCES member (id)
@@ -183,10 +184,27 @@ CREATE TABLE recipe_favorite
 CREATE TABLE recipe_score
 (
     id        BIGINT AUTO_INCREMENT PRIMARY KEY,
-    member_id bigint  NOT NULL,
-    score     int NOT NULL,
-    recipe_id bigint  NOT NULL,
+    member_id bigint NOT NULL,
+    score     int    NOT NULL,
+    recipe_id bigint NOT NULL,
     CONSTRAINT FK_RECIPE_SCORE_MEMBER
+        FOREIGN KEY (member_id) REFERENCES member (id)
+            ON DELETE CASCADE
+);
+
+
+-- cloth_member
+CREATE TABLE cloth_member
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    cloth_id   BIGINT   NOT NULL,
+    member_id  BIGINT   NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    updated_at DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    CONSTRAINT FK_CLOTH_ID
+        FOREIGN KEY (cloth_id) REFERENCES cloth (id)
+            ON DELETE CASCADE,
+    CONSTRAINT FK_MEMBER_ID
         FOREIGN KEY (member_id) REFERENCES member (id)
             ON DELETE CASCADE
 );
